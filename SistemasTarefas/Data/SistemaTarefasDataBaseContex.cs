9f77e4fd5e854cd667706b40cb1465acc20574ef
Leonardo Romeiro <leozinwork@gmail.com>
2023-09-06 13:09:48 -0300

    Substituição de repositorio

---
+++ SistemasTarefas/Data/SistemaTarefasDataBaseContex.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using SistemasTarefas.Models;
+
+namespace SistemasTarefas.Data
+{
+    public class SistemaTarefasDataBaseContex : DbContext
+    {
+        public SistemaTarefasDataBaseContex(DbContextOptions<SistemaTarefasDataBaseContex> options) :base(options)
+        {
+             
+        }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SistemasTarefas/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasTarefas.Data;
+using SistemasTarefas.Models;
+using SistemasTarefas.Repositorios.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace SistemasTarefas.Repositorios
+{
+	public class UsuarioRepositorio:IUsuarioRepositorio
+	{
+		private readonly SistemaTarefasDataBaseContex baseContex;
+		public UsuarioRepositorio(SistemaTarefasDataBaseContex sistemaTarefasDataBaseContex) {
+			baseContex = sistemaTarefasDataBaseContex;
+		}
+
+		public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+		{
+			baseContex.Usuarios.Add(usuario);
+			baseContex.SaveChanges();
+			return usuario;
+		}
+
+		public async Task<bool> Apagar(int id)
+		{
+            UsuarioModel usuarioModel = await BuscarPorId(id);
+            if (usuarioModel == null)
+            {
+                throw new Exception($"Usuario para o ID:{id}, não foi encontrado ou não existe no banco de dados");
+            }
+                      baseContex.Usuarios.Update(usuarioModel);
+            baseContex.SaveChanges();
+            return true;
+        }
+
+		public async Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+		{
+			UsuarioModel usuarioModel = await BuscarPorId(id);
+			if (usuarioModel == null)
+			{
+				throw new Exception($"Usuario para o ID:{id}, não foi encontrado ou não existe no banco de dados");
+			}
+			usuarioModel.Name = usuario.Name;
+            usuarioModel.Email = usuario.Email;
+			baseContex.Usuarios.Update(usuarioModel);
+            baseContex.SaveChanges();
+			return usuarioModel;
+        }
+
+		public async Task<UsuarioModel> BuscarPorId(int id)
+		{
+            return await baseContex.Usuarios.FirstOrDefaultAsync(x => x.Id == id);//x => x.Id == id é uma função lambda, ou seja, definir uma função anônima ou um delegado. 
+			//nesse caso a lambda representa um critério de filtragem ou projeção.  x representa ela, => indica q é lambda, x.Id == id expressão booleana que a função lambda retorna
+        }
+
+		public async Task<List<UsuarioModel>> BuscarTodosUsuarios()
+		{
+            return await baseContex.Usuarios.ToListAsync();
+        }
+	}
+}
